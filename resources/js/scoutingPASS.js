6612d13787ce1488ff3dc6e8c2a5be189faad6c0
Team-6220 <122236965+Team-6220@users.noreply.github.com>
2023-01-17 16:57:54 -0800

    Update scoutingPASS.js

--- resources/js/scoutingPASS.js
+++ resources/js/scoutingPASS.js
@@ -44,7 +44,7 @@ function addTimer(table, idx, name, data){
   button1.setAttribute("id", "start_"+data.code);
   button1.setAttribute("type", "checkbox");
   button1.setAttribute("onclick", "timer(this.parentElement)");
-  //button1.innerHTML += "Start"
+  button1.innerHTML += "Start"
   cell2.appendChild(button1);
 
   var inp = document.createElement("input");
