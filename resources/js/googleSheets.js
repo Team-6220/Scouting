c4c7775b48f7ecce2c9405bffc823b55bc7a8a0c
Team-6220 <122236965+Team-6220@users.noreply.github.com>
2023-01-21 10:02:16 -0800

    Update googleSheets.js

--- resources/js/googleSheets.js
+++ resources/js/googleSheets.js
@@ -6,9 +6,6 @@ function sendToGoogleSheets(){
     const scriptURL = 'https://script.google.com/macros/s/AKfycbwLMqEvVQYSSxeAL58DGDdNPwcDPOUhPRbErxkIYY_ElKVK6vv0umVDW5z5WM6d07k/exec'
     const form = document.querySelector('#scoutingForm')
     const btn = document.querySelector('#submit')
- 
-      btn.disabled = true
-      btn.innerHTML = "Sending..."
 
       let fd = getData(false)
       for (const [key, value] of fd) {
