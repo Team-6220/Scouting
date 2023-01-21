fe4e3a80766ac75cced3caa1d1cc04400c7b013d
Team-6220 <122236965+Team-6220@users.noreply.github.com>
2023-01-21 09:32:11 -0800

    Update googleSheets.js

--- resources/js/googleSheets.js
+++ resources/js/googleSheets.js
@@ -19,6 +19,7 @@ function setUpGoogleSheets() {
               alert('Success!', response) })
         .catch(error => {
               alert('Error!', error.message)})
+      console.log("Sent to Google Sheets")
 
       btn.disabled = false
       btn.innerHTML = "Send to Google Sheets"
