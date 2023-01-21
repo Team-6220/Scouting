ead87aa34523580d926ed0f4cc646aa8a02b8b23
Team-6220 <122236965+Team-6220@users.noreply.github.com>
2023-01-21 10:07:15 -0800

    Update googleSheets.js

--- resources/js/googleSheets.js
+++ resources/js/googleSheets.js
@@ -9,9 +9,6 @@ function setUpGoogleSheets() {
 }
 
 function sendToGoogleSheets(){
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbwLMqEvVQYSSxeAL58DGDdNPwcDPOUhPRbErxkIYY_ElKVK6vv0umVDW5z5WM6d07k/exec'
-    const form = document.querySelector('#scoutingForm')
-    const btn = document.querySelector('#submit')
 
       let fd = getData(false)
       for (const [key, value] of fd) {
